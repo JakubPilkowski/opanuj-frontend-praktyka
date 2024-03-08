f33849c28164e7532fec564bbdee34f6bd345150
Jakub Piłkowski <pilkowskijakub@gmail.com>
2024-03-08 11:01:48 +0100

    Revert "Implement application using react query revalidation and optimistic u…"

--- examples/scripts/middlewares/data-middleware.mjs
+++ examples/scripts/middlewares/data-middleware.mjs
@@ -2,8 +2,6 @@ import { readFile } from 'fs/promises';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import { writeFileSync } from 'fs';
-import { z } from 'zod';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -49,67 +47,6 @@ export const createDataMiddleware = () => {
   router.post('/:resource', async (req, res) => {
     return res.sendStatus(200);
   });
-
-  router.post(
-    '/add-item',
-    async (req, res, next) => {
-      const itemSchema = z
-        .object({
-          body: z
-            .object({
-              name: z
-                .string()
-                .trim()
-                .toLowerCase()
-                .min(1, 'Name cannot be empty')
-                .max(100, 'Name cannot exceed 100 words'),
-            })
-            .strict(),
-        })
-        .strict();
-      try {
-        const result = await itemSchema.parseAsync({
-          body: req.body,
-        });
-        req.body = result.body;
-        return next();
-      } catch (error) {
-        return res.status(400).json(error);
-      }
-    },
-    async (req, res) => {
-      const resourceName = 'items';
-      const resource = await readContent(resourceName);
-
-      const items = new Set(JSON.parse(resource).items);
-
-      const name = req.body.name;
-
-      const oldLength = items.size;
-      items.add(name);
-
-      const expectedLength = oldLength + 1;
-
-      try {
-        const newItems = z
-          .set(z.string())
-          .size(expectedLength, 'Item already exists')
-          .parse(items);
-
-        const obj = { items: Array.from(newItems) };
-
-        writeFileSync(
-          join(__dirname, `../data/${resourceName}.json`),
-          JSON.stringify(obj, null, 2),
-          () => {}
-        );
-
-        return res.json({ data: req.body.name });
-      } catch (err) {
-        return res.status(400).json(err);
-      }
-    }
-  );
 
   return router;
 };
